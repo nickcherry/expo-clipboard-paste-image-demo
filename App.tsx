011cfdc7027b7c9d3f8d3664ece0acc2fe6cacfe
Nick Cherry <nickcherryjiggz@gmail.com>
2022-07-05 15:07:36 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Does clipboard have an image?"
+        onPress={async () => {
+          const hasImage = await Clipboard.hasImageAsync();
+          Alert.alert(hasImage ? 'Image found!' : 'No image found :(');
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
