118e30a03dd5db8aa4267b1ffc586ddb629225b2
Nick Cherry <nickcherryjiggz@gmail.com>
2022-07-05 15:32:17 -0400

    show image if present

--- App.tsx
+++ App.tsx
@@ -1,18 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
+import { useState } from 'react';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState<string>();
+
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <Button
         title="Does clipboard have an image?"
         onPress={async () => {
           const hasImage = await Clipboard.hasImageAsync();
-          Alert.alert(hasImage ? 'Image found!' : 'No image found :(');
+
+          if (hasImage) {
+            Alert.alert('Image found!');
+            const clipboardImage = await Clipboard.getImageAsync({
+              format: 'png',
+            });
+
+            if (clipboardImage) {
+              setImageUri(clipboardImage.data);
+            }
+          } else {
+            Alert.alert('No image found :(');
+            setImageUri(undefined);
+          }
         }}
       />
-      <StatusBar style="auto" />
+      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
     </View>
   );
 }
@@ -23,5 +40,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    aspectRatio: 1,
+    marginTop: 20,
+    width: 200,
   },
 });
