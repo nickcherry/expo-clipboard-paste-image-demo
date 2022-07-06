bac42ed3193cddceeb821576e8fb8baf1b4cbba4
Nick Cherry <nickcherryjiggz@gmail.com>
2022-07-05 22:00:35 -0400

    show clipboard state

--- App.tsx
+++ App.tsx
@@ -1,37 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import { AppState, StyleSheet, Text, View } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+interface ClipboardState {
+  hasString: boolean;
+  string: string | null;
+  hasUrl: boolean;
+  url: string | null;
+  hasImage: boolean;
+  imageUri: string | null;
+}
+
+const formatValue = (value: string | boolean) => {
+  if (typeof value === 'boolean') {
+    return value ? 'true' : 'false';
+  }
+
+  return value || '–';
+};
 
 export default function App() {
-  const [imageUri, setImageUri] = useState<string>();
+  const [clipboardState, setClipboardState] = useState<ClipboardState>();
+
+  const updateClipboardState = useCallback(async () => {
+    const hasString = await Clipboard.hasStringAsync();
+    const string = await Clipboard.getStringAsync();
+
+    const hasUrl = await Clipboard.hasUrlAsync();
+    const url = await Clipboard.getUrlAsync();
+
+    const hasImage = await Clipboard.hasImageAsync();
+    const imageResult = await Clipboard.getImageAsync({ format: 'jpeg' });
+
+    setClipboardState({
+      hasString,
+      string,
+      hasUrl,
+      url,
+      hasImage,
+      imageUri: imageResult ? imageResult.data : null,
+    });
+  }, []);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', async (status) => {
+      if (status === 'active') {
+        updateClipboardState();
+      }
+    });
+
+    updateClipboardState();
+
+    return () => subscription.remove();
+  }, []);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Button
-        title="Does clipboard have an image?"
-        onPress={async () => {
-          setImageUri(undefined);
-
-          const hasImage = await Clipboard.hasImageAsync();
-
-          if (hasImage) {
-            Alert.alert('Image found!');
-
-            const clipboardImage = await Clipboard.getImageAsync({
-              format: 'png',
-            });
-
-            if (clipboardImage) {
-              setImageUri(clipboardImage.data);
-            }
-          } else {
-            Alert.alert('No image found :(');
-          }
-        }}
-      />
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      {Object.entries(clipboardState || {}).map(([key, value]) => (
+        <View style={styles.dataRow}>
+          <Text style={styles.dataRowLabel}>{key}</Text>
+          <Text style={styles.dataRowValue} numberOfLines={1}>
+            {formatValue(value)}
+          </Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -43,9 +77,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    aspectRatio: 1,
-    marginTop: 20,
-    width: 200,
+  dataRow: {
+    marginBottom: 40,
+    paddingHorizontal: 20,
+  },
+  dataRowLabel: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  dataRowValue: {
+    textAlign: 'center',
   },
 });
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # expo-clipboard paste image demo
 
-Install dependencies by running `yarn install`, then start the iOS app by running `yarn ios`. Copy an image to the clipboard from another app, then return to the demo app and press the "Does clipboard have an image?" button.
+Install dependencies by running `yarn install`, then start the iOS app by running `yarn ios`. Copy an image to the clipboard from another app, then return to the demo app to view the state of the clipboard.
 
 See [./demo.mov](./demo.mov) for a pre-recorded demonstration of the problem.
